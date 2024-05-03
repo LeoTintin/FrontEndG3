2d60ea8762107486f63ad31651876917006160ac
iFelppz <felipecaue7@hotmail.com>
2024-05-03 16:47:41 -0300

    feat: finalizando desc e js

---
+++ vingadores/scripts.js
@@ -0,0 +1,9 @@
+function showDescription(colunalink) {
+    var descricao = colunaLink.querySelector('.desc');
+    descricao.style.opacity = '1';
+}
+
+function hideDescription(colunalink) {
+    var descricao = colunaLink.querySelector('.desc');
+    descricao.style.opacity = '0';
+}
